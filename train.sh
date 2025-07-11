ffdefe5af72316100d9cc90979467b609aaceb61
山丘 <81578796+Wild-star@users.noreply.github.com>
2025-07-11 22:32:49 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -18,3 +18,7 @@ python train.py --epochs 10000 --batch_size 128 --hidden_size 64 --n_runs 3 --se
 
 python train_pro.py --epochs 10000 --batch_size 128 --hidden_size 64 --n_runs 3 --seq_len 365 \
 --pred_len 365
+
+python train_tran.py --epochs 1500 --batch_size 256 --lr 0.0001 --seq_len 90 --pred_len 90
+
+python train_tran.py --epochs 1500 --batch_size 256 --lr 0.0001 --seq_len 90 --pred_len 365
